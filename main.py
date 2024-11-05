beccbc483089b8e039284e15ffc01abea7bb2668
Mana Poustizadeh <mana0pst@gmail.com>
2024-11-04 22:25:29 -0500

    create CNN

---
+++ main.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        # create convolutional layers
+        # conv1: 1 channel (grayscale), outputs 6 channels, and uses a 5x5 filter.
+        # conv2: 6 channels from conv1's output, outputs 16 channels, and uses a 5x5 filter.
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5*5 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    # For a step-by-step guide, follow the readme file
+    def forward(self, input):
+        # Convolution layer C1: 1 input image channel, 6 output channels,
+        # 5x5 square convolution, it uses RELU activation function, and
+        # outputs a Tensor with size (N, 6, 28, 28), where N is the size of the batch
+        c1 = F.relu(self.conv1(input))
+        # Subsampling layer S2: 2x2 grid
+        s2 = F.max_pool2d(c1, (2, 2))
+        c3 = F.relu(self.conv2(s2))
+        s4 = F.max_pool2d(c3, 2)
+        # Flatten operation: outputs a (N, 400) Tensor
+        s4 = torch.flatten(s4, 1)
+        f5 = F.relu(self.fc1(s4))
+        f6 = F.relu(self.fc2(f5))
+        # Gaussian layer OUTPUT: (N, 84) Tensor input, and
+        # outputs a (N, 10) Tensor
+        output = self.fc3(f6)
+        return output
