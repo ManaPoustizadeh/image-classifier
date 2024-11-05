df4859b393d0aff917685fbd28f20ef93fc2caa5
Mana Poustizadeh <mana0pst@gmail.com>
2024-11-05 18:39:19 -0500

    add train and test + last touches

--- classifier/main.py
+++ classifier/main.py
@@ -6,32 +6,133 @@ import torchvision.transforms as transforms
 
 from classifier.cnn import Net
 
+PATH = "./cifar_net.pth"
+
+
 class ClassificationConfig:
     def __init__(self):
         self.transform = transforms.Compose(
-        [transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
         self.batch_size: int = 4
+        self.epoch_size: int = 2
+
 
 class Classifier:
     def __init__(self):
         self.config = ClassificationConfig()
-        self.load_data(self, self.config.transform, self.config.batch_size)
         self.net = Net()
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = optim.SGD(self.net.parameters(), lr=0.001, momentum=0.9)
-    
-    def load_data(self, transform, batch_size: int):
 
-        self.trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                                download=True, transform=transform)
-        self.trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=batch_size,
-                                                shuffle=True, num_workers=2)
+    def load_data(self):
+        batch_size = self.config.batch_size
+        self.trainset = torchvision.datasets.CIFAR10(
+            root="./data", train=True, download=True, transform=self.config.transform
+        )
+        self.trainloader = torch.utils.data.DataLoader(
+            self.trainset, batch_size=batch_size, shuffle=True, num_workers=2
+        )
 
-        self.testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                            download=True, transform=transform)
-        self.testloader = torch.utils.data.DataLoader(self.testset, batch_size=batch_size,
-                                                shuffle=False, num_workers=2)
+        self.testset = torchvision.datasets.CIFAR10(
+            root="./data", train=False, download=True, transform=self.config.transform
+        )
+        self.testloader = torch.utils.data.DataLoader(
+            self.testset, batch_size=batch_size, shuffle=False, num_workers=2
+        )
 
-        self.classes = ('plane', 'car', 'bird', 'cat',
-                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        self.classes = (
+            "plane",
+            "car",
+            "bird",
+            "cat",
+            "deer",
+            "dog",
+            "frog",
+            "horse",
+            "ship",
+            "truck",
+        )
+
+    def train_network(self):
+        for epoch in range(
+            self.config.epoch_size
+        ):  # loop over the dataset multiple times
+
+            running_loss = 0.0
+            for i, data in enumerate(self.trainloader, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data
+
+                # zero the parameter gradients
+                self.optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = self.net(inputs)
+                loss = self.criterion(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+                if i % 2000 == 1999:  # print every 2000 mini-batches
+                    print(f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}")
+                    running_loss = 0.0
+
+        print("Finished Training")
+
+    def test_network(self):
+        correct = 0
+        total = 0
+        # since we're not training, we don't need to calculate the gradients for our outputs
+        with torch.no_grad():
+            for data in self.testloader:
+                images, labels = data
+                # calculate outputs by running images through the network
+                outputs = self.net(images)
+                # the class with the highest energy is what we choose as prediction
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        print(
+            f"Accuracy of the network on the 10000 test images: {100 * correct // total} %"
+        )
+
+    def analyze_results(self):
+        correct_pred = {classname: 0 for classname in self.classes}
+        total_pred = {classname: 0 for classname in self.classes}
+
+        # again no gradients needed
+        with torch.no_grad():
+            for data in self.testloader:
+                images, labels = data
+                outputs = self.net(images)
+                _, predictions = torch.max(outputs, 1)
+                # collect the correct predictions for each class
+                for label, prediction in zip(labels, predictions):
+                    if label == prediction:
+                        correct_pred[self.classes[label]] += 1
+                    total_pred[self.classes[label]] += 1
+
+        # print accuracy for each class
+        for classname, correct_count in correct_pred.items():
+            accuracy = 100 * float(correct_count) / total_pred[classname]
+            print(f"Accuracy for class: {classname:5s} is {accuracy:.1f} %")
+
+    def save_trained_model(self):
+        torch.save(self.net.state_dict(), PATH)
+
+    def save_trained_model(self):
+        self.net.load_state_dict(torch.load(PATH, weights_only=True))
+
+
+if __name__ == "__main__":
+    classifier = Classifier()
+    classifier.load_data()
+    classifier.train_network()
+    classifier.test_network()
+    classifier.analyze_results
